acb3720a92ad343b1a2982695353108de4c9d86c
Rishi Jain <27rishijainpersonalemail@gmail.com>
2026-06-13 20:44:28 -0400

    Updated soln to prob 15

--- 15_linear_regression_using_gradient_descent.py
+++ 15_linear_regression_using_gradient_descent.py
@@ -13,26 +13,42 @@ def linear_regression_gradient_descent(X, y, alpha, iterations) -> torch.Tensor:
     Returns:
         Learned weights as a 1D tensor of shape (n,)
     """
-    X_t = torch.as_tensor(X, dtype=torch.float32)
-    y_t = torch.as_tensor(y, dtype=torch.float32).reshape(-1, 1)
+    # Convert X and y to tensors
+    X_t = torch.as_tensor(X, dtype=torch.float32) # (m, n)
+    y_t = torch.as_tensor(y, dtype=torch.float32).reshape(-1, 1) # (m, 1)
+
+    # Create theta
     m, n = X_t.shape
-    theta = torch.zeros((n, 1), requires_grad=True) # weights & biases
-    
+    theta = torch.zeros((n, 1), requires_grad=True)
+    # theta = (n, 1)
+    # X_t @ theta = shape of (m, 1)
+    # That can be used in a loss calculation with y_t which has
+    # shape of (m, 1)
+
+    # Training loop
+    # Loop iterations times
     for epoch in range(iterations):
         # 1. Zero grad
+        # optimizer.zero_grad()
         if theta.grad is not None:
             theta.grad.zero_()
+
         # 2. Forward pass
-        y_pred = X_t @ theta
+        prediction = X_t @ theta
+
         # 3. Calc loss
-        loss = (1/(2*m)) * torch.sum((y_pred - y_t)**2)
-        # 4. Backprop
+        loss = (1/(2 * m)) * torch.sum((prediction - y_t)**2)
+
+        # 4. Backpropagation
         loss.backward()
+
         # 5. Gradient descent
+        # optimizer.step()
         with torch.no_grad():
-            if theta is not None:
-                theta -= alpha * theta.grad
-        
-    
+            if theta.grad is not None:
+                theta -= (alpha * theta.grad)
+
+    # Return theta (weights and biases)
+    theta = theta.squeeze(1)
     return theta.detach()
 
